febbd1276e21208225562ebbb6fe8a86123d0c38
AbdulRehman <112686679+Abdul-Rehman7826@users.noreply.github.com>
2024-01-30 23:08:35 +0500

    "CSV To XLSX Save as"

--- src/App.jsx
+++ src/App.jsx
@@ -120,27 +120,26 @@ function App() {
         "Email",
       ],
     ];
-    // console.log(...arr);
     data.push(...newArr);
-
-    const csvContent = data.map((row) => row.join(";")).join("\n");
-    console.log(csvContent);
-
-    const blob = new Blob([csvContent], { type: "text/xlsx" });
-
+    // Create a new workbook
+    const workbook = XLSX.utils.book_new();
+    const worksheet = XLSX.utils.aoa_to_sheet(data);
+    XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
+    const excelBuffer = XLSX.write(workbook, {
+      bookType: "xlsx",
+      type: "array",
+    });
+    const blob = new Blob([excelBuffer], { type: "application/octet-stream" });
     const url = URL.createObjectURL(blob);
-
     const a = document.createElement("a");
     a.href = url;
-    a.download = "data.csv";
+    a.download = "data.xlsx";
     a.style.display = "none";
-
     document.body.appendChild(a);
-
     a.click();
-
     document.body.removeChild(a);
     URL.revokeObjectURL(url);
+
     console.log("Done ! ");
   };
 
